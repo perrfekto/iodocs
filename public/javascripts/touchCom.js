2b258078973e137b213b9eae234a94ef410891ce
perrfekto <riana@perrfekto.com>
2012-10-25 12:27:21 +0400

    Array add and remove javascript

--- public/javascripts/touchCom.js
+++ public/javascripts/touchCom.js
@@ -1,6 +1,6 @@
-function addRow (obj, nmItem, numEl) {
-  var pre = nmItem + '[' + numEl + ']';
-  var parent = nmItem.split('.').pop();
+function addRow (obj, nameItem, numEl) {
+  var pre = nameItem + '.' + numEl;
+  var parent = nameItem.split('.').pop();
   if (obj.hasOwnProperty ('Name')) {
     var name = obj.Name;
   }
@@ -8,11 +8,11 @@ function addRow (obj, nmItem, numEl) {
     var name = '';
   }
 
-  var trStr = '<tr id="' + pre + '" data-item="' + nmItem;
+  var trStr = '<tr id="tr_' + pre + '.' + name + '" data-item="' + nameItem + '" class="indent1';
   if (obj.hasOwnProperty ('Required') && obj.Required.toLowerCase() == 'y') {
-    trStr += ' class="required"';
+    trStr += ' required';
   }
-  trStr += '>';
+  trStr += '">';
   
   trStr += '<td class="name">' + name + '</td>';
 
@@ -47,13 +47,13 @@ function addRow (obj, nmItem, numEl) {
 
   // TODO: dataRow and update of json
   else if (typ == 'array') {
-    trStr += '<a data-item="' + nmItem + '[' + numEl + '] data-row="0", data-num="0" class="addItems">++</a>';
+    trStr += '<a data-item="' + nameItem + '[' + numEl + ']" data-row="0", data-num="0" class="addItems">++</a>';
   }
       
   else {     
     trStr += '<input name="reqbody[' + parent + '][' + numEl + '][' + name + ']"/>';
   }
-  trStr += '<td>';
+  trStr += '</td>';
 
   trStr += '<td class="type">' + typ + '</td>';
   trStr += '<td class="description"><p>';
@@ -72,26 +72,108 @@ function addRow (obj, nmItem, numEl) {
     var typesArray = JSON.parse (jsStr);
   }
 
-  $('a.addItems').live('click', function() {
-    var nmItem = $(this).attr('data-item');
-    var numEl = $(this).attr('data-num');
-    var rowNum = $(this).attr('data-row');
-    var trStr = '';
 
-    if (typesArray[nmItem]) {
-      var len = typesArray[nmItem].length;
+  $('a.addItems').live('click', function() {
+    var nameItem = $(this).attr('data-item');
+    var numEl = $(this).attr('data-num');
+    var numItems = $(this).attr('data-numItems');
+    var rowNum = parseInt ($(this).attr('data-row')) + (parseInt (numEl) * (parseInt (numItems) + 1)) - 1;
+    var a = nameItem.split('.');
+    var dispName = a.pop() + '[' + numEl + ']';
+    var tblName = '#tbl\\.' + a.join('\\.') + '\\.body';
+
+    // Add rows
+    var trStr = '<tr id="tr_' + nameItem + '.' + numEl + 
+      '." class="indent1head"><td id="td_' + nameItem + '.' + numEl + '.">' + dispName + 
+      '</td><td><a id="a_' + nameItem + '.' + numEl + '." class="delItems" data-item="' + nameItem + '" data-seq="' + numEl + '">--</a></td><td></td><td></td></tr>';
+
+    if (typesArray[nameItem]) {
+      var len = typesArray[nameItem].length;
 
       for (var i = 0; i < len; i++) {
-        trStr += addRow (typesArray[nmItem][i], nmItem, parseInt(numEl) + i);
+        trStr += addRow (typesArray[nameItem][i], nameItem, numEl);
       }
     }
 
-    var a = nmItem.split('.');
-    a.pop();
-    var tblName = 'tbl.' + a.join('.') + '.body';
-    $('#tbl.Members.Test.body > tbody:last').append (trStr);
-//    alert (trStr);
+    $(tblName + ' > tbody > tr').eq (rowNum).after(trStr);
     
+    // Update buttons's data-num attribute
+    $(this).attr ('data-num', parseInt (numEl) + 1);
+
   });
+
+
+  $('a.delItems').live('click', function() {
+    var nameItem = $(this).attr('data-item');
+    var seq = $(this).attr('data-seq');
+    var parent = nameItem.split('.').pop();
+
+    // Remove the rows
+    var sel = 'tr[id^="tr_' + nameItem.replace (/\./g, '\\.') + '\\.' + seq + '"]';
+    $(sel).remove();
+
+    sel = '#addItems\\.' + nameItem.replace (/\./g, '\\.');
+    var numEl = $(sel).attr('data-num');
+    $(sel).attr('data-num', (parseInt (numEl) - 1));
+
+    // Update remaining rows' ids and form fields' names to reflect new seq
+
+    seq = parseInt (seq);
+    var repl = '';
+    var tmp = '';
+    var ii = 0;
+
+    for (var i = seq + 1; i < parseInt (numEl); i++) {
+      ii = parseInt (i) - 1;
+      sel = '*[name^="reqbody[' + parent + '][' + i + ']"]';
+      src = 'reqbody[' + parent + '][' + i + ']';
+      repl = 'reqbody[' + parent + '][' + ii + ']';
+
+      $(sel).each (function () {
+        tmp = $(this).attr('name');
+        tmp = tmp.replace (src, repl);
+         
+        $(this).attr('name', tmp);
+      });
+
+
+      sel = 'tr[id^="tr_' + nameItem.replace (/\./g, '\\.') + '\\.' + i + '\\."]';
+      src = 'tr_' + nameItem + '.' + i + '.';
+      repl = 'tr_' + nameItem + '.' + ii + '.';
+
+      $(sel).each (function () {
+        tmp = $(this).attr('id');
+        tmp = tmp.replace (src, repl);
+         
+        $(this).attr('id', tmp);
+      });
+
+
+      sel = 'td[id^="td_' + nameItem.replace (/\./g, '\\.') + '\\.' + i + '\\."]';
+      src = 'td_' + nameItem + '.' + i + '.';
+      repl = 'td_' + nameItem + '.' + ii + '.';
+
+      $(sel).each (function () {
+        tmp = $(this).attr('id');
+        tmp = tmp.replace (src, repl);
+         
+        $(this).attr('id', tmp);
+        $(this).html(parent + '[' + ii + ']');
+      });
+
+
+      sel = 'a[id^="a_' + nameItem.replace (/\./g, '\\.') + '\\.' + i + '\\."]';
+      src = 'a_' + nameItem + '.' + i + '.';
+      repl = 'a_' + nameItem + '.' + ii + '.';
+      $(sel).each (function () {
+        tmp = $(this).attr('id');
+        tmp = tmp.replace (src, repl);
+         
+        $(this).attr('id', tmp);
+        $(this).attr('data-seq', ii);
+      });
+    }
+  });
+
 })();
 
